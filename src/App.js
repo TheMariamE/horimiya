3733cd32e910ae80ee68ceee5a5f8736e98c7d68
TheMariamE <mariamedwards.smart@gmail.com>
2021-07-31 16:05:23 -0500

    Added first section - synopsis

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from "./components/Navbar";
 import MainBanner from './components/MainBanner';
+import Synopsis from './components/Synopsis';
 
 function App() {
   return (
     <>
       <Navbar />
       <MainBanner />
+      <Synopsis />
     </>
   );
 }
---
+++ src/components/Synopsis.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import mainbg from "../mainbg.jpg";
+
+const Synopsis = () => {
+    return (
+        <div id="synopsis" className="container-lg synopsis">
+            <div className="row">
+                <div className="col-sm-12">
+                    <h2 className="s-title">Synopsis</h2>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-sm-6 s-description">
+                    <div>
+                        <p>Kyouko Hori seems like an average teenage girl, but she has a different side outside of school that she wants no one else to ever find out about. Brought up in a household where both her parents are absent from working, she needs to take care of the house and her little brother, unable to lead a normal social life. Likewise, there is her classmate Izumi Miyamura, who has a different side out of school that remains hidden from others. When the two meet unexpectedly, they discover each others' secrets and develop a special friendship.</p>
+                    </div>
+                </div>
+                <div className="col-sm-6 s-img">
+                    <div>
+                        <img src={mainbg} alt="Horimiya - Main Background"></img>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Synopsis
