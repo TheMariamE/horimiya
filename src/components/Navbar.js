89d403e48e5da4db71cf5587d54aa2bcfb399e53
TheMariamE <mariamedwards.smart@gmail.com>
2021-07-30 23:59:56 -0500

    Adding fonts and style to header

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,17 +1,18 @@
-import React from 'react'
+import React from 'react';
+import logo from "../horimiya.png";
 
 const Navbar = () => {
     return (
-    <div className="col-sm-7">
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <a className="navbar-brand" href="#">Navbar</a>
+    <div className="col-sm-10">
+        <nav id="h-menu" className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="#"><img src={logo} alt="Horimiya - Logo"></img></a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarNavDropdown">
                 <ul className="navbar-nav">
                 <li className="nav-item active">
-                    <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+                    <a className="nav-link" href="#">Home</a>
                 </li>
                 <li className="nav-item">
                     <a className="nav-link" href="#">About Us</a>
