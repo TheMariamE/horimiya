743c1c6deefd595d8fc38e9836cdaaf4e858bde7
TheMariamE <mariamedwards.smart@gmail.com>
2021-08-18 00:12:55 -0500

    Added other page Banners

---
+++ src/components/AuthorBanner.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const AuthorBanner = () => {
+    return (
+        <div>
+            <div className="author-bg">
+                <div className="col-md-11 main-title about-title">
+                    <h1 className="h-title">Authors</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default AuthorBanner;
--- src/pages/Author.js
+++ src/pages/Author.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Navbar from "../components/Navbar";
 import Footer from '../components/Footer';
+import AuthorBanner from '../components/AuthorBanner';
 
 const Author = () => {
     return (
         <div id="author">
             <Navbar />
-            <h1>AUTHOR</h1>
+            <AuthorBanner />
             <Footer /> 
         </div>
     )
