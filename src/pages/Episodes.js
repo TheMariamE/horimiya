b0bc87e7770fccc00a03f6e36683be7d842c96e5
TheMariamE <mariamedwards.smart@gmail.com>
2021-08-30 20:31:34 -0500

    Created Player Component

--- src/pages/Episodes.js
+++ src/pages/Episodes.js
@@ -2,12 +2,35 @@ import React from 'react';
 import Navbar from "../components/Navbar";
 import Footer from '../components/Footer';
 import EpisodesBanner from '../components/EpisodesBanner';
+import readmanga from "../components/imgs/ReadtheManga.jpg";
+import ep1 from "../components/imgs/episode1.jpg";
+import player from "../components/imgs/play-button.svg";
 
 const Episodes = () => {
     return (
         <div id="episodes">
             <Navbar />
             <EpisodesBanner />
+            <div className="container-fluid dark-bg-ep">
+                <div className="container ep-container">
+                    <div className="row ep">
+                        <div className="col-md-3">
+                            <img class="ep-img" src={ep1} alt="Episode 1 - Horimiya"></img>
+                        </div>
+                        <div className="col-md-9 ep-content">
+                            <div className="row">
+                                <div className="col-md-9">
+                                    <h2 className="ep-title">Episode 1 - A Tiny Happenstance</h2>
+                                    <p className="ep-date">Air Date: February 6, 2021</p>
+                                </div>
+                                <div className="col-md-3">
+                                    <img class="ep-player" src={player} alt="Episode 1 - Horimiya"></img>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
             <a href="https://readmanganato.com/manga-rz951534" id="rm-btn" rel="noreferrer" target="_blank">
                 <div id="read-manga" className="container-fluid blue-bg-rm">
                     <div className="container rm-container">
@@ -16,7 +39,7 @@ const Episodes = () => {
                                 <h2 class="rm-txt">Read the Manga Now Online!</h2>                    
                             </div>
                             <div className="col-md-3 btn-col">
-                            <img class="rm-btn" src={readmanga} alt="Read Manga - Horimiya"></img>
+                                <img class="rm-btn" src={readmanga} alt="Read Manga - Horimiya"></img>
                             </div>
                         </div>
                     </div>
