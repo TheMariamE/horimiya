6d03917e53765f54a9eec8e1a39a552b0136b596
TheMariamE <mariamedwards.smart@gmail.com>
2021-08-31 23:33:56 -0500

    Added Complete Episodes Content

--- src/components/EpisodesList.js
+++ src/components/EpisodesList.js
@@ -4,39 +4,86 @@ import ep2 from "./imgs/episode2.jpg";
 import ep3 from "./imgs/episode3.jpg";
 import ep4 from "./imgs/episode4.jpg";
 import ep5 from "./imgs/episode5.jpg";
+import ep6 from "./imgs/episode6.jpg";
+import ep7 from "./imgs/episode7.jpg";
+import ep8 from "./imgs/episode8.jpg";
+import ep9 from "./imgs/episode9.jpg";
+import ep10 from "./imgs/episode10.jpg";
+import ep11 from "./imgs/episode11.jpg";
+import ep12 from "./imgs/episode12.jpg";
+import ep13 from "./imgs/episode13.jpg";
+import ep14 from "./imgs/episode14.jpg";
 
 const EpisodesList = [
 {
-    url: "https://www.youtube.com/watch?v=bdDTSn8ZhxQ",
     title: "Episode 1 - A Tiny Happenstance",
     date: "Air Date: February 6, 2021",
     img1: ep1,
     mEpisode: "https://mega.nz/embed/6IsDESwI#WtlCmHPdt_lhT2MAKz07RLNGxybPxJDv3A8KxjTkzOw",
-    url2: "https://www.youtube.com/watch?v=IDnr1yfnSgQ",
     title2: "Episode 2 - You Wear More Than One Face",
     date2: "Air Date: February 13, 2021",
     mEpisode2: "https://mega.nz/embed/uI0nwIIa#dh_NfbmC_8iGW9oe6u0vxGGImTyQu9HSfoKh8BYkhv0",
     img2: ep2
 },
 {
-    url: "https://www.youtube.com/watch?v=WU96UtqHQ6A",
     title: "Episode 3 - That's Why It's Okay",
     date: "Air Date: February 20, 2021",
     img1: ep3,
-    url2: "https://www.youtube.com/watch?v=Xa8AvSrg_Gg",
+    mEpisode: "https://mega.nz/embed/eMtRnQzS#jU6PPlYrW6ecJmWDmER98ZClGRwW_wrJ4MeCae8jD3U",
     title2: "Episode 4 - Everybody Loves Somebody",
     date2: "Air Date: March 6, 2021",
+    mEpisode2: "https://mega.nz/embed/LFVQxZLJ#VEGJjFnhBJJUuHTIxqefVAbT_I3zJhY1BT0wTjog9Vw",
     img2: ep4
 },
 {
-    url: "https://www.youtube.com/watch?v=S6SMNCloEI4",
     title: "Episode 5 - I Can't Say It Out Loud",
     date: "Air Date: March 13, 2021",
     img1: ep5,
-    url2: "https://www.youtube.com/watch?v=IDnr1yfnSgQ",
-    title2: "Episode 6 - You Wear More Than One Face",
+    mEpisode: "https://mega.nz/embed/EAQlmaaR#DmueZQdJBdeJMrGK4Jtwdhw-QNVrk_9TDLGx2w-tuts",
+    title2: "Episode 6 - This Summer's Going to Be a Hot One",
     date2: "Air Date: March 20, 2021",
-    img2: ep5
+    mEpisode2: "https://mega.nz/embed/UYRHDY4J#_cQbnoprepVLLFCEnvoW9XTbPSLl6M71Lh8BvXGZn4Q",
+    img2: ep6
+},
+{
+    title: "Episode 7 - You're Here, I'm Here",
+    date: "Air Date: March 27, 2021",
+    img1: ep7,
+    mEpisode: "https://mega.nz/embed/ZIRFxQhD#Syhe1wj5AHS_inAkIozuJCCubtzHI6CEW5FFIL0YD0E",
+    title2: "Episode 8 - The Truth Deception Reveals",
+    date2: "Air Date: April 3, 2021",
+    mEpisode2: "https://mega.nz/embed/BUZXWSpS#V6Y912f6y38gsgdQearAnQBEnqPpif0F1KTBs4HZe7c",
+    img2: ep8
+},
+{
+    title: "Episode 9 - It's Hard, but Not Impossible",
+    date: "Air Date: April 10, 2021",
+    img1: ep9,
+    mEpisode: "https://mega.nz/embed/ANJRjC5a#HnRrOUkr330SHu0iSFjTyY_bF1P8AVSfmZUYfZ1q-JY",
+    title2: "Episode 10 - Until the Snow Melts",
+    date2: "Air Date: April 17, 2021",
+    mEpisode2: "https://mega.nz/embed/hMRzlAIB#ktuIz0XZDgES8uYMNQrPzaj9-Yo3GkBB_0YMvPAr95I",
+    img2: ep10
+},
+{
+    title: "Episode 11 - It May Seem Like Hate",
+    date: "Air Date: April 24, 2021",
+    img1: ep11,
+    mEpisode: "https://mega.nz/embed/ZJRVCIpZ#EUTZfYM-mxBcs_8cIXXMr734ovzIMnNWgK2_cmFWD7g",
+    title2: "Episode 12 - Hitherto, and Forevermore",
+    date2: "Air Date: May 1, 2021",
+    mEpisode2: "https://mega.nz/embed/8cQxEITJ#9XkyW5gb6-P17iadvnbQwP97ZEsQ2pJolDBrZWSBLQI",
+    img2: ep12
+},
+{
+    title: "Episode 13 - I Would Gift You the Sky",
+    date: "Air Date: May 8, 2021",
+    img1: ep13,
+    mEpisode: "https://mega.nz/embed/4AAngaDS#oLaxuawMBX13kZN6OoKEnpMrbCo6nLLspfoMbYIZDzs",
+    title2: "Episode 14 - Never Going To Let You Down",
+    date2: "Air Date: May 15, 2021",
+    mEpisode2: "https://www.youtube.com/embed/dQw4w9WgXcQ",
+    img2: ep14
 }
 ];
 export default EpisodesList;
