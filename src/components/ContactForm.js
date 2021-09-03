be1d9c4f57fcc3dc17eee36d4195c76ec4932884
TheMariamE <mariamedwards.smart@gmail.com>
2021-09-02 23:31:10 -0500

    Adjustments to Contact Section

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import emailjs from 'emailjs-com';
-import { Form, Input, TextArea, Button } from 'semantic-ui-react';
+import { Form, Input, TextArea, Button, Select } from 'semantic-ui-react';
 import Swal from 'sweetalert2';
 
 const SERVICE_ID = "service_s9ofxys";
@@ -27,6 +27,22 @@ const ContactForm = () => {
           });
         e.target.reset()
       };
+      const episodeOptions = [
+        { key: 'ep1', value: 'ep1', text: 'Episode 1 - A Tiny Happenstance' },
+        { key: 'ep2', value: 'ep2', text: 'Episode 2 - You Wear More Than One Face' },
+        { key: 'ep3', value: 'ep3', text: "Episode 3 - That's Why It's Okay" },
+        { key: 'ep4', value: 'ep4', text: 'Episode 4 - Everybody Loves Somebody' },
+        { key: 'ep5', value: 'ep5', text: "Episode 5 - I Can't Say It Out Loud" },
+        { key: 'ep6', value: 'ep6', text: "Episode 6 - This Summer's Going to Be a Hot One" },
+        { key: 'ep7', value: 'ep7', text: "Episode 7 - You're Here, I'm Here" },
+        { key: 'ep8', value: 'ep8', text: 'Episode 8 - The Truth Deception Reveals' },
+        { key: 'ep9', value: 'ep9', text: "Episode 9 - It's Hard, but Not Impossible" },
+        { key: 'ep10', value: 'ep10', text: "Episode 10 - Until the Snow Melts" },
+        { key: 'ep11', value: 'ep11', text: "Episode 11 - It May Seem Like Hate" },
+        { key: 'ep12', value: 'ep12', text: "Episode 12 - Hitherto, and Forevermore" },
+        { key: 'ep13', value: 'ep13', text: "Episode 13 - I Would Gift You the Sky" },
+
+      ]
 
 return (
     <div className="ContactForm">
@@ -36,44 +52,32 @@ return (
             id='form-input-control-name'
             className="col-md-6"
             control={Input}
-            label='Full Name'
             name='user_name'
-            placeholder='Name…'
+            placeholder='Full Name*'
             required
             />
             <Form.Field
             id='form-input-control-email'
             className="col-md-6"
             control={Input}
-            label='Email'
             name='user_email'
-            placeholder='Email…'
+            placeholder='Email*'
             required
             />
             <Form.Field
             id='form-input-control-phone'
             className="col-md-6"
             control={Input}
-            label='Phone Number'
             name='user_phone'
-            placeholder=''
+            placeholder='Phone Number*'
             required
             />
-            <Form.Field
-            id='form-input-control-country'
-            className="col-md-6"
-            control={Input}
-            label='Country'
-            name='user_country'
-            placeholder=''
-            required
-            />
+            <Select id='form-input-control-country' className="col-md-6" name="user_episode" placeholder="Select favorite episode" options={episodeOptions} required />
             <Form.Field
             id='form-textarea-control-opinion'
             control={TextArea}
-            label='Message'
             name='user_message'
-            placeholder='Message…'
+            placeholder='Message*'
             required
             />
             <Button type='submit' color='green'>Submit</Button>
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar";
 import Footer from '../components/Footer';
 import ContactBanner from '../components/ContactBanner';
 import ContactForm from '../components/ContactForm';
-import facebook from "../components/imgs/facebook.png";
+import facebook from "../components/imgs/c-facebook.png";
 import linkedin from "../components/imgs/linkedin.png";
 import youtube from "../components/imgs/youtube.png";
 import twitter from "../components/imgs/twitter.png";
@@ -22,21 +22,27 @@ const Contact = () => {
                                 <a className="social-logo" href="https://www.facebook.com/Horimiya.animefanpage/" target="_blank" rel="noreferrer"><img src={facebook} alt="Facebook - Logo"></img></a>
                             </div>
                             <div className="col-md-3">
-                                <a className="social-logo" href="https://www.linkedin.com/in/mariam-edwards/" target="_blank" rel="noreferrer"><img src={linkedin} alt="Linkedin - Logo"></img></a>
+                                <a className="social-logo" href="https://www.linkedin.com/in/mariam-edwards/" target="_blank" rel="noreferrer"><img src={facebook} alt="Linkedin - Logo"></img></a>
                             </div>
                             <div className="col-md-3">
-                                <a className="social-logo" href="https://www.youtube.com/watch?v=8iP8xXXdvoU" target="_blank" rel="noreferrer"><img src={youtube} alt="Youtube - Logo"></img></a>
+                                <a className="social-logo" href="https://www.youtube.com/watch?v=8iP8xXXdvoU" target="_blank" rel="noreferrer"><img src={facebook} alt="Youtube - Logo"></img></a>
                             </div>
                             <div className="col-md-3">
-                                <a className="social-logo" href="https://twitter.com/horimiya_anime" target="_blank" rel="noreferrer"><img src={twitter} alt="Twitter - Logo"></img></a>
+                                <a className="social-logo" href="https://twitter.com/horimiya_anime" target="_blank" rel="noreferrer"><img src={facebook} alt="Twitter - Logo"></img></a>
                             </div>
                         </div>
                     </div>  
                 </div>                    
             </div>
-            <div className="container-fluid contact-c">
+            <div id="contact-form" className="container-fluid">
                 <div className="container">
-                    <ContactForm />
+                    <div className="row">  
+                        <div className="col-md-6">
+                            <ContactForm />
+                        </div>
+                        <div className="col-md-6">
+                        </div>
+                    </div>
                 </div>
             </div>
             <Footer /> 
