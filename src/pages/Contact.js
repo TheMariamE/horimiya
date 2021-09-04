adb5f7ae3ad3282e59f66894ed10bf5db56786a7
TheMariamE <mariamedwards.smart@gmail.com>
2021-09-04 00:44:06 -0500

    Added Contact Logos, Changed format

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,10 +3,10 @@ import Navbar from "../components/Navbar";
 import Footer from '../components/Footer';
 import ContactBanner from '../components/ContactBanner';
 import ContactForm from '../components/ContactForm';
-import facebook from "../components/imgs/c-facebook.png";
-import linkedin from "../components/imgs/linkedin.png";
-import youtube from "../components/imgs/youtube.png";
-import twitter from "../components/imgs/twitter.png";
+import facebook from "../components/imgs/facebook.svg";
+import linkedin from "../components/imgs/linkedin.svg";
+import youtube from "../components/imgs/youtube.svg";
+import twitter from "../components/imgs/twitter.svg";
 
 const Contact = () => {
     return (
@@ -22,13 +22,13 @@ const Contact = () => {
                                 <a className="social-logo" href="https://www.facebook.com/Horimiya.animefanpage/" target="_blank" rel="noreferrer"><img src={facebook} alt="Facebook - Logo"></img></a>
                             </div>
                             <div className="col-md-3">
-                                <a className="social-logo" href="https://www.linkedin.com/in/mariam-edwards/" target="_blank" rel="noreferrer"><img src={facebook} alt="Linkedin - Logo"></img></a>
+                                <a className="social-logo" href="https://www.linkedin.com/in/mariam-edwards/" target="_blank" rel="noreferrer"><img src={linkedin} alt="Linkedin - Logo"></img></a>
                             </div>
                             <div className="col-md-3">
-                                <a className="social-logo" href="https://www.youtube.com/watch?v=8iP8xXXdvoU" target="_blank" rel="noreferrer"><img src={facebook} alt="Youtube - Logo"></img></a>
+                                <a className="social-logo" href="https://www.youtube.com/watch?v=8iP8xXXdvoU" target="_blank" rel="noreferrer"><img src={youtube} alt="Youtube - Logo"></img></a>
                             </div>
                             <div className="col-md-3">
-                                <a className="social-logo" href="https://twitter.com/horimiya_anime" target="_blank" rel="noreferrer"><img src={facebook} alt="Twitter - Logo"></img></a>
+                                <a className="social-logo" href="https://twitter.com/horimiya_anime" target="_blank" rel="noreferrer"><img src={twitter} alt="Twitter - Logo"></img></a>
                             </div>
                         </div>
                     </div>  
@@ -44,7 +44,7 @@ const Contact = () => {
                             <ContactForm />
                         </div>
                         <div className="col-md-6">
-                        <iframe className="map-horimiya" src="https://www.google.com/maps/d/u/0/embed?mid=1Us4MPiCRfUFe-IBQg68EkzW5o4K5uVxH" height="480"></iframe>
+                        <iframe className="map-horimiya" src="https://www.google.com/maps/d/u/0/embed?mid=1Us4MPiCRfUFe-IBQg68EkzW5o4K5uVxH" height="750"></iframe>
                         </div>
                     </div>
                 </div>
