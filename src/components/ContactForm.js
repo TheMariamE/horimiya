2da1c9dea6bd3f2e5d73b21f13c997c98e4ea68e
TheMariamE <mariamedwards.smart@gmail.com>
2021-09-04 16:06:44 -0500

    Email validation fix pt1

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import emailjs from 'emailjs-com';
 import { Form, Input, TextArea, Button, Select } from 'semantic-ui-react';
 import Swal from 'sweetalert2';
+import EmailValidator from 'email-validator';
+
 
 const SERVICE_ID = "service_s9ofxys";
 const TEMPLATE_ID = "template_4s92d5a";
@@ -27,20 +29,22 @@ const ContactForm = () => {
           });
         e.target.reset()
       };
+      const [emailInput, setEmail] = useState("");
+      const [validEmail, validateEmail] = useState(true);
       const episodeOptions = [
-        { key: 'ep1', value: 'ep1', text: 'Episode 1 - A Tiny Happenstance' },
-        { key: 'ep2', value: 'ep2', text: 'Episode 2 - You Wear More Than One Face' },
-        { key: 'ep3', value: 'ep3', text: "Episode 3 - That's Why It's Okay" },
-        { key: 'ep4', value: 'ep4', text: 'Episode 4 - Everybody Loves Somebody' },
-        { key: 'ep5', value: 'ep5', text: "Episode 5 - I Can't Say It Out Loud" },
-        { key: 'ep6', value: 'ep6', text: "Episode 6 - This Summer's Going to Be a Hot One" },
-        { key: 'ep7', value: 'ep7', text: "Episode 7 - You're Here, I'm Here" },
-        { key: 'ep8', value: 'ep8', text: 'Episode 8 - The Truth Deception Reveals' },
-        { key: 'ep9', value: 'ep9', text: "Episode 9 - It's Hard, but Not Impossible" },
-        { key: 'ep10', value: 'ep10', text: "Episode 10 - Until the Snow Melts" },
-        { key: 'ep11', value: 'ep11', text: "Episode 11 - It May Seem Like Hate" },
-        { key: 'ep12', value: 'ep12', text: "Episode 12 - Hitherto, and Forevermore" },
-        { key: 'ep13', value: 'ep13', text: "Episode 13 - I Would Gift You the Sky" }
+        { key: 'ep1', value: "Episode 1 - A Tiny Happenstance", text: 'Episode 1 - A Tiny Happenstance' },
+        { key: 'ep2', value: "Episode 2 - You Wear More Than One Face", text: 'Episode 2 - You Wear More Than One Face' },
+        { key: 'ep3', value: "Episode 3 - That's Why It's Okay", text: "Episode 3 - That's Why It's Okay" },
+        { key: 'ep4', value: "Episode 4 - Everybody Loves Somebody", text: 'Episode 4 - Everybody Loves Somebody' },
+        { key: 'ep5', value: "Episode 5 - I Can't Say It Out Loud", text: "Episode 5 - I Can't Say It Out Loud" },
+        { key: 'ep6', value: "Episode 6 - This Summer's Going to Be a Hot One", text: "Episode 6 - This Summer's Going to Be a Hot One" },
+        { key: 'ep7', value: "Episode 7 - You're Here, I'm Here", text: "Episode 7 - You're Here, I'm Here" },
+        { key: 'ep8', value: "Episode 8 - The Truth Deception Reveals", text: 'Episode 8 - The Truth Deception Reveals' },
+        { key: 'ep9', value: "Episode 9 - It's Hard, but Not Impossible", text: "Episode 9 - It's Hard, but Not Impossible" },
+        { key: 'ep10', value: "Episode 10 - Until the Snow Melts", text: "Episode 10 - Until the Snow Melts" },
+        { key: 'ep11', value: "Episode 11 - It May Seem Like Hate", text: "Episode 11 - It May Seem Like Hate" },
+        { key: 'ep12', value: "Episode 12 - Hitherto, and Forevermore", text: "Episode 12 - Hitherto, and Forevermore" },
+        { key: 'ep13', value: "Episode 13 - I Would Gift You the Sky", text: "Episode 13 - I Would Gift You the Sky" }
       ]
  
 return (
@@ -62,6 +66,15 @@ return (
             name='user_email'
             placeholder='Email*'
             required
+            value={emailInput}
+            onChange={e => {
+                setEmail(e.target.value);                               
+                validateEmail(EmailValidator.validate(e.target.value));
+            }}
+            error={validEmail ? false : {
+                content: 'Please enter a valid email address.',
+                pointing: 'below'
+            }}
             />
             <Form.Field
             id='form-input-control-phone'
@@ -71,9 +84,10 @@ return (
             placeholder='Phone Number*'
             required
             />
-            <Select 
+            <Form.Field 
             id='form-input-control-country' 
             className="col-md-6" 
+            control={Select}
             options={episodeOptions} 
             name='user_episode'
             placeholder="Select favorite episode" 
