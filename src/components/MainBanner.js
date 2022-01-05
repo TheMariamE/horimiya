46fc0224d817f4f1254e847fb8c47234ad48fb11
TheMariamE <mariamedwards.smart@gmail.com>
2022-01-04 20:00:04 -0500

    Responsive adjustments to home

--- src/components/MainBanner.js
+++ src/components/MainBanner.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import mainbg from "./imgs/mainbg.jpg";
+// import mainbg from "./imgs/mainbg.jpg";
 
 const MainBanner = () => {
     return (
@@ -7,27 +7,26 @@ const MainBanner = () => {
             <div className="col-sm-12">
                 <h2 className="jap-title">ホリミヤ</h2>
             </div>
-            <div className="col-md-11 main-title">
-                <h1 className="h-title">Horimiya</h1>
-            </div>
             <div className="main-bg">
-                <img src={mainbg} alt="Horimiya - Main Background"></img>
-            </div>
-            <div className="char-cards">
-                <ul className="row">
-                <li className="col-12 col-md-6 col-lg-6">
-                    <div className="cnt-block equal-hight kh">
-                        <h3>Kyouko Hori</h3>
-                        <p><a href="/about">Learn More!</a></p>
-                    </div>
-                </li>
-                <li className="col-12 col-md-6 col-lg-6">
-                    <div className="cnt-block equal-hight im">
-                        <h3>Izumi Miyamura</h3>
-                        <p><a href="/about">Learn More!</a></p>
-                    </div>
-                </li>
-                </ul>
+                <div className="col-md-11 main-title">
+                    <h1 className="h-title">Horimiya</h1>
+                </div>
+                <div className="char-cards">
+                    <ul className="row">
+                    <li className="col-12 col-md-6 col-lg-6">
+                        <div className="cnt-block equal-hight kh">
+                            <h3>Kyouko Hori</h3>
+                            <p><a href="/about">Learn More!</a></p>
+                        </div>
+                    </li>
+                    <li className="col-12 col-md-6 col-lg-6">
+                        <div className="cnt-block equal-hight im">
+                            <h3>Izumi Miyamura</h3>
+                            <p><a href="/about">Learn More!</a></p>
+                        </div>
+                    </li>
+                    </ul>
+                </div>
             </div>    
         </div>
     )
